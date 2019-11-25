0c8707cfaa12c9212a2275a681b01a35c862aae0
T.Ito <your.email@example.com>
2019-11-25 03:59:20 +0000

    hp6

--- app/controllers/portals_controller.rb
+++ app/controllers/portals_controller.rb
@@ -1,7 +1,24 @@
 class PortalsController < ApplicationController
   def index
+    @portal=Portal.find(1)
   end
 
   def new
+    @portal=Portal.new
+  end
+  def create
+    @portal = Portal.new(portal_params)
+    @portal.save
+  end
+  def edit
+    @portal=Portal.find(params[:id])
+  end
+  def update
+    @portal=Portal.update(portal_params)
+    redirect_to('/')
+  end
+  private
+  def portal_params
+     params.require(:portal).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'portals/index'
-
-  get 'portals/new'
-
+  
+  
+  #get 'portals/:id/edit'
   get '/'=>'portals#index'
-
+  
+  resources :portals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
