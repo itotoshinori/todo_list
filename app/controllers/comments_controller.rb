ba0b84cc1252d5201c6ffb88fbea711f15e8ba58
T.Ito <your.email@example.com>
2019-12-04 07:36:45 +0000

    hp252

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,26 @@
 class CommentsController < ApplicationController
   def create
-    @comments=Comment.new(comment_params)
-    @comments.save
-    redirect_to request.referer
+    @comment=Comment.new(comment_params)
+    blog_id=@comment.blog_id
+    if @comment.save
+        redirect_to "/blogs/#{blog_id.to_s}"
+    else
+      @blog=Blog.find(blog_id)
+      @name=@comment.name
+      @body=@comment.body
+      
+      #redirect_to "/commens/comments_error"
+      render comment_error_path
+      #render "/commens/comments_error"
+      #redirect_to "/blogs/#{blog_id.to_s}"
+    end
+  end
+  def comment_error
+     
+    
+  end
+  def test
+    
   end
   def comment_params
      params.require(:comment).permit(:name,:body,:blog_id)
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   resources :blogs
   #get 'portals/:id/edit'
   get '/'=>'portals#index'
-  post 'blogs/commentcreate'
-  resources :comments
+  #get '/comments/comment_error'
+  post '/comments/comment_error', as: :comment_error
+  #post '/comments/test'
+  get '/comments/test', as: :test
+  #post 'blogs/commentcreate'
+  resources :comments, only: [:create]
   resources :portals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
