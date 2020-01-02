34031f998dd76b0cb84dd2e52e50c7641f3bd38b
T.Ito <your.email@example.com>
2020-01-02 13:37:44 +0900

    todo-4-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,8 @@
 class BlogsController < ApplicationController
+  before_action :userid_set,only: [:new,:create,:edit,:update]
+  
   def index
     @blogs=Blog.paginate(page: params[:page], per_page: 5).order(created_at: "DESC")
-    
   end
 
   def new
@@ -29,8 +30,16 @@ class BlogsController < ApplicationController
     @comment=Comment.new
   end
   
-
+private
   def blog_params
      params.require(:blog).permit(:title, :content,:postdate)
+  end
+  def userid_set
+    if cookies[:userid].present?
+      @userid=cookies[:userid].to_i
+      cookies[:userid] = {:value => @userid, :expires => 5.days.from_now }
+    else
+      redirect_to('/login/index') 
+    end
   end
 end
