9183fdac957ffcdf5208a83388bac41579774682
T.Ito <your.email@example.com>
2020-08-16 10:48:20 +0900

    edit_todo_show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,28 @@
 class ApplicationController < ActionController::Base
-   
   protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
   include ApplicationHelper
   helper_method :current_user
-  
+
   def userid_set
     if cookies[:userid].present?
       @userid=cookies[:userid].to_i
       #current_user
       cookies[:userid] = {:value => @userid, :expires => 5.days.from_now }
     else
-      redirect_to('/login/index') 
+      redirect_to('/login/index')
     end
   end
   def unless_user
     @todo=Todo.includes(:accounts).find(params[:id])
     if cookies[:userid] != @todo.user_id
       redirect_to('/login/index')
+    end
+  end
+
+  def unless_admin_user
+    if cookies[:userid].present?
+      redirect_to('/login/index') if cookies[:userid] != "1"
     end
   end
 end
