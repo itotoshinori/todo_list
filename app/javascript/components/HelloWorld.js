ec7e60b2d39b304c042dd9fd33dc88c2c73b4626
vagrant <vagrant@localhost.localdomain>
2020-09-06 00:25:01 +0000

    react-rails

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -5,33 +5,25 @@ var today = new Date();
 var todayDay = today.getMonth() + 1
 var dayOfWeek = today.getDay() ;
 var dayOfWeekStr = [ "日", "月", "火", "水", "木", "金", "土" ][dayOfWeek] ;
-var asastu
-
 class HelloWorld extends React.Component {
-    constructor( props )
-  {
-    super( props );
-    this.state = { isPubleshed: true }
-    this.state = { count: 0 }
-    this.statePrice = { price: 3280 }
+   constructor(props) {
+    super(props);
+    this.state = {aisatu:"こんにちわ"}
   }
-
   render () {
     let thisday = today.getFullYear() + "年" + todayDay   + "月"+ today.getDate()  + "日"
-    let time = today.getHours() 
-    let  aisatu = "おはようございます"
-    if(time >= 10){
-      aisatu = "こんにちは"
-    } else if(time >= 19){
-      aisatu = "こんばんは"
+    let greeting = this.state.aisatu
+     if(today.getHours() >=10){
+      greeting = "こんにちわ"
+    } else if ( today.getHours() >=19){
+      greeting =　"こんばんわ"
     }
-    
+   
     return (
       <React.Fragment>
-        
-     {aisatu } 本日は{ thisday }({dayOfWeekStr})です
-        
-      </React.Fragment>
+      { greeting }
+       本日は{ thisday }({dayOfWeekStr})です
+     </React.Fragment>
       
     );
   }
