f60c4823f1d7e1839f0a0d3436e6f2491ffd067e
vagrant <vagrant@localhost.localdomain>
2020-09-18 08:07:36 +0000

    分類番号集計

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   validates :todo_id, presence: true
   validates :category_id, presence: true
+    
   enum category_id: { 医療: 1, 自宅: 2, 旅行: 3,プログラム:4,スポーツ:5,他:6 }
 
   def category_insert(categories,todo_id)
@@ -11,5 +12,18 @@ class Category < ApplicationRecord
     end
     result
   end
+
+  def category_select_id(category_id)
+    categories = Category.order(:category_id)
+    @category_ids = []
+    category_id2 = nil
+    categories.each do |category|
+      if  category.category_id != category_id2
+        @category_ids << category.category_id
+      end
+      category_id2 = category.category_id
+    end
+    @category_ids
+  end  
 end
   
