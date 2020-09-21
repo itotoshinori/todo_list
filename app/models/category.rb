ecc40db56e1c115609c2ce5d88a69679749d8367
vagrant <vagrant@localhost.localdomain>
2020-09-21 03:21:04 +0000

    カテゴリ作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   validates :todo_id, presence: true
   validates :category_id, presence: true
     
-  enum category_id: { 医療: 1, 薬: 2, 旅行: 3,プログラム:4,映画・テレビ:5,自宅:6,保険・財テク:7, スポーツ: 2 }
+  enum category_id: { 医療: 1, 薬: 2, 検査結果: 3,プログラム:4,映画・テレビ:5,自宅:6,保険・財テク:7, スポーツ: 8, 旅行:9,ブックマーク:10 }
 
   def category_insert(categories,todo_id)
     result = false
