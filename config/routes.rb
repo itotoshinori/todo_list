cc1676359d928add052d3efa5bf53cbf1b96812e
T-Ito <vagrant@localhost.localdomain>
2020-10-15 12:35:41 +0000

    ルーツ修正

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,5 @@ Rails.application.routes.draw do
   get '/'=>'portals#index'
   post '/comments/comment_error', as: :comment_error
   get '/comments/test', as: :test
-  
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
