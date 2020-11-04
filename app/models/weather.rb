4e130b4ba5066f0640e0569b5a2013b6c8717abf
Itotoshinori <tito40358@gmail.com>
2020-11-04 15:45:31 +0000

    weater_setting

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -5,15 +5,12 @@ class Weather
     json = Net::HTTP.get(uri) #NET::HTTPを利用してAPIを叩く
     result = JSON.parse(json) #返り値をubyの配列に変
     url1 = result['forecasts'][0]['image']['url']
-    date1 = result['forecasts'][0]['date']
     url2 = result['forecasts'][1]['image']['url']
-    date2 = result['forecasts'][1]['date']
     url3 = result['forecasts'][2]['image']['url']
-    date3 = result['forecasts'][2]['date']
     dis = ""
-    day = day.to_s
     dis = []
     dis << url1
     dis << url2
+    dis << url3
   end
 end
