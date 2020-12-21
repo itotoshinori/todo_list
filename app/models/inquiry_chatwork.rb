146a7af3ae162ba5ca240943abe90291a83955d7
itotoshinori <tito40358@gmail.com>
2020-12-21 19:01:32 +0900

    Update inquiry_chatwork.rb

--- app/models/inquiry_chatwork.rb
+++ app/models/inquiry_chatwork.rb
@@ -4,30 +4,6 @@ class InquiryChatwork
   require 'json'
   #git hubでの編集要
   def push_chatwork_message(message,num)
-    chatwork_room_id = '197330662'
-    chatwork_api_token = '74bfe242711fcf573a2379c183b250fb'
-    uri = URI.parse("https://api.chatwork.com/v2/rooms/#{chatwork_room_id}/messages")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    if message.present? and num <=2
-      if num == 1
-        title = message.blog.title
-        messagetext = "[To:3775224]#{message.name}さん(#{message.email})から「#{title}」のブログにコメントがありました。確認をお願いします。"
-      elsif num == 2
-        title ="ログインされました"
-        messagetext = "[To:3775224]ユーザー#{message.name}にログインがありました。心当たりがなければ、パスワードを変更して下さい。"
-      end
-      http.start do
-        req = Net::HTTP::Post.new(uri.path)
-        req['X-ChatWorkToken'] = chatwork_api_token
-        req.set_form_data(body: messagetext)
-        http.request(req) 
-        result = true
-      end
-    else
-      result = false
-    end
-  end
+  end  
 end
 
