57666d73f31f0b80e61f8aa5910f993937ea8793
T-Ito <example@test.com>
2020-12-22 17:12:03 +0900

    RSpecでtodosコントローラー修正

--- spec/requests/todos_request_spec.rb
+++ spec/requests/todos_request_spec.rb
@@ -4,18 +4,24 @@ RSpec.describe TodosController, type: :request do
   before do
     @user = FactoryBot.create(:user)
     @todo = @user.todos.create(
-      title: "テスト",
+      title: "コロナ対策",
       term: 2020-10-22)
     @account = @todo.accounts.create(
       amount: 100)
-    @userid = 1
   end
+
   describe "#index" do
-   # 正常にレスポンスを返すこと
+    # ログインしてなければログイン画面に遷移する
     it "responds successfully" do
-      post todos_url
+      cookies[:userid] = nil
+      post todos_path
       expect(response).to redirect_to login_index_path
-      #expect(response).to redirect_to todos_path　←こちらで通したいがエラーがでる
+    end
+   # ログインしていれば、/todosに遷移すること
+    it "responds successfully" do
+      cookies[:userid] = "1" #自作ログインメソッドでこれでログイン状態にはなる模様
+      post todos_path
+      expect(response).to redirect_to todos_path #←こちらをexamples（緑）で通したいがfailures（赤）がでる
     end
   end
 end
